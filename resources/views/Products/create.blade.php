d0a74a04337b3d0cb2a149b62dd9eb7214e906f4
ibnubachdar <ibnubachdar@gmail.com>
2025-12-07 16:20:15 +0700

    add feature additional images for product

--- resources/views/Products/create.blade.php
+++ resources/views/Products/create.blade.php
@@ -13,8 +13,7 @@
                 <label>Product Name</label>
                 <div class="flex gap-x-5">
                     <input type="text" name="name[]" required
-                    class="border focus:border-blue-500 outline-none px-3 py-1">
-                    <p id="toggleAdd" class="font-semibold bg-yellow-500 px-3 py-1 cursor-pointer">Add</p>
+                    class="border focus:border-blue-500 outline-none px-3 py-1 w-full">
                 </div>
                 @error('name.*')
                     <p class="text-red-500">{{ $message }}</p>
@@ -26,7 +25,18 @@
                     <p class="text-red-500">{{ $message }}</p>
                 @enderror
 
+                <div id="addImage">
+                    <label>Additional Image</label>
+                    <div class="flex">
+                        <input type="file" name="filename[]">
+                        <p id="toggleAdd" class="font-semibold bg-yellow-500 px-3 py-1 cursor-pointer">Add</p>
+                    </div>
+                </div>
                 
+                @error('filename.*')
+                    <p class="text-red-500">{{ $message }}</p>
+                @enderror
+
             </div>
 
             <div class="text-center">
@@ -37,28 +47,64 @@
     </div>
 
     <script>
+        const formImage = document.getElementById('inputBar');
+        const addImage = document.getElementById('addImage');
+
         const toggleAdd = document.getElementById('toggleAdd');
-        const inputBar = document.getElementById('inputBar');
-        const form = document.getElementById('form');
+        // const toggleClose = document.getElementById('toggleClose');
 
         toggleAdd.addEventListener('click', function(e) {
             e.preventDefault();
 
-            const panjangInput = document.querySelectorAll('input[name="name[]"]').length;
-            if (panjangInput > 2) {
-                alert('Maksimal 2 product saja.'); 
-                return; 
+            if (document.querySelectorAll('#addImage').length > 3) {
+                alert('Maksimal foto tambahan hanya 4');
+                return;
             }
 
-            const clone = inputBar.cloneNode(true);
-            clone.querySelector('input').value = '';
+            const clone = addImage.cloneNode(true);
 
-            const cloneAdd = clone.querySelector('#toggleAdd');
-            if (cloneAdd) cloneAdd.remove();
+            const cloneToggleAdd = clone.querySelector('#toggleAdd');
+            if (cloneToggleAdd) cloneToggleAdd.remove();
 
-            // sisipkan setelah inputBar
-            inputBar.insertAdjacentElement('afterend', clone)
+
+            formImage.insertAdjacentElement('afterend', clone)
         });
 
+
+
     </script>
+    
 @endsection
+
+
+
+
+
+<script>
+    // ======================== DUMP
+
+
+//     const toggleAdd = document.getElementById('toggleAdd');
+    //     const inputBar = document.getElementById('inputBar');
+    //     const form = document.getElementById('form');
+
+    //     toggleAdd.addEventListener('click', function(e) {
+    //         e.preventDefault();
+
+    //         const panjangInput = document.querySelectorAll('input[name="name[]"]').length;
+    //         if (panjangInput > 2) {
+    //             alert('Maksimal 2 product saja.'); 
+    //             return; 
+    //         }
+
+    //         const clone = inputBar.cloneNode(true);
+    //         clone.querySelector('input').value = '';
+
+    //         const cloneAdd = clone.querySelector('#toggleAdd');
+    //         if (cloneAdd) cloneAdd.remove();
+
+    //         // sisipkan setelah inputBar
+    //         inputBar.insertAdjacentElement('afterend', clone)
+    //     });
+</script>
+    
