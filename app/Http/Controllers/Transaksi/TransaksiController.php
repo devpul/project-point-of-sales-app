4271de53356321b99f6ff9eac05cfd5a873af2da
ibnubachdar <ibnubachdar@gmail.com>
2025-12-07 23:11:02 +0700

    menambah fitur buat transaksi penjualan 1 produk

--- app/Http/Controllers/Transaksi/TransaksiController.php
+++ app/Http/Controllers/Transaksi/TransaksiController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\Transaksi;
 
+use App\Models\Customers;
+use App\Models\Outlets;
 use App\Models\ProductStock;
 use App\Models\SaleItems;
 use App\Models\Sales;
 use App\Models\Products;
+use App\Models\Shifts;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -18,14 +21,16 @@ class TransaksiController extends Controller
         try {
 
             $rules_store = [
-                'invoice_code'      =>  'required|string',
+                // products
+                'product_id'        =>  'required|exists:products,id',
+
+                // 'invoice_code'      =>  'required|string',
                 'customer_id'       =>  'required|exists:customers,id',
                 'outlet_id'         =>  'required|exists:outlets,id',
-                'shift_id'          =>  'required|exists:shifts,id',
-                'discount'          =>  'nullable|numeric|min:0',
+                // 'shift_id'          =>  'required|exists:shifts,id',
+                // 'discount'          =>  'nullable|numeric|min:0',
                 'method'            =>  'required|in:cash,qris',
                 'paid_amount'       =>  'required|numeric|min:0',
-                'cash_change'       =>  'required|numeric|min:0',
                 'status'            =>  'required|in:pending,paid,cancel,refund',
                 'receipt_method'    =>  'required|in:print,email',
                 'description'       =>  'nullable|string',
@@ -40,9 +45,7 @@ class TransaksiController extends Controller
                 'rules_update'   =>  $rules_update,
             ];
 
-            if (isset($rules[$action])) {
-                return $request->validate($rules[$action]);
-            }
+            if (isset($rules[$action])) return $request->validate($rules[$action]);
 
             return back()->with('error', 'Terjadi Kesalahan Pada Server.');
 
@@ -53,39 +56,84 @@ class TransaksiController extends Controller
 
     public function store(Request $request)
     {
+        // dd($request->all());
+
+        $userId = Auth::user()->id;
+
         $validated = $this->validateTransaction($request, 'rules_store');
         if (! is_array($validated)) return $validated;
-        
+    
+        // SHIFT WOII !!
+        Shifts::find($userId);
+
+
+        $product = Products::find($validated['product_id']);
+
+        $totalSementara = $product->price * $validated['qty'];
+
+        // cek kalo ada promo / diskon
+        if ($product->has_promo === 'yes') {
+
+            if ($product->promo_type === 'percentage') {
+
+                $grandTotal = $product->price - ($totalSementara * $product->promo_amount);
+
+            }
+
+            if ($product->promo_type === 'fixed_amount') {
+
+                $grandTotal = $totalSementara - $product->promo_amount;
+                
+            }
+
+            $discount = $product->amount;
+        } else {
+            $grandTotal = $totalSementara;
+        }
+
+
+        // cek kalo duit kurang
+        if ($validated['paid_amount'] < $grandTotal) {
+            return back()->with('error', 'Maaf duit kamu kurang.');
+        } 
+
+        // kalo duit lebih, berarti kembalian
+        if ($validated['paid_amount'] > $grandTotal) {
+            $cash_change = $validated['paid_amount'] - $grandTotal;
+        }
 
         $sale = Sales::create([
             'user_id'           =>  Auth::user()->id,
-            'invoice_code'      =>  str_pad('INV ', 4, '0000', STR_PAD_LEFT),
-            'customer_id'       =>  $validated['invoice_code'],
-            'outlet_id'         =>  $validated['invoice_code'],
-            'shift_id'          =>  $validated['invoice_code'],
-            'discount'          =>  $validated['invoice_code'],
-            'method'            =>  $validated['invoice_code'],
-            'paid_amount'       =>  $validated['invoice_code'],
-            'cash_change'       =>  $validated['invoice_code'],
-            'status'            =>  $validated['invoice_code'],
-            'receipt_method'    =>  $validated['invoice_code'],
-            'description'       =>  $validated['invoice_code'],
+            'customer_id'       =>  $validated['customer_id'],
+            'outlet_id'         =>  $validated['outlet_id'],
+            // 'shift_id'          =>  $validated['shift_id'],
+            'discount'          =>  $discount ?? null,
+            'method'            =>  $validated['method'],
+            'paid_amount'       =>  $validated['paid_amount'],
+            'total_amount'      =>  $grandTotal,
+            'cash_change'       =>  $cash_change,
+            'status'            =>  $validated['status'],
+            'receipt_method'    =>  $validated['receipt_method'],
+            'description'       =>  $validated['description'],
         ]);
 
-        $total = $validated['price'] * $validated['qty'];
+        $sale->update([
+            'invoice_code'      =>  'INV-' . date('Y'). '-' . str_pad($sale->id, 4, '0', STR_PAD_LEFT)
+        ]);
 
         $item = SaleItems::create([
             'sale_id'       =>  $sale->id,
             'product_id'    =>  $validated['product_id'],
             'qty'           =>  $validated['qty'],
-            'price'         =>  $validated['price'],
-            'total'         =>  $total
+            'price'         =>  $product->price,
+            'total'         =>  $grandTotal
         ]);
 
-        // kurangi qty
-        // ProductStock::where('product_id', $validated['product_id'])
-        //             ->decrement('qty', $validated['qty']);
+        // kurang stok produk otomatis
+        ProductStock::where('product_id', $validated['product_id'])
+                ->decrement('qty', $validated['qty']);
         
+        return redirect()->route('penjualan.index')->with('success', 'Transaksi baru berhasil dibuat.');
     }
 
     public function index()
@@ -98,8 +146,12 @@ class TransaksiController extends Controller
     public function create()
     {
         $products = Products::distinct()->orderBy('name')->get();
-        if ($products->isEmpty()) return back()->with('error', 'Produk tidak ditemukan.');
+
+        $customers = Customers::get();
+
+        $outlets = Outlets::get();
+        // if ($products->isEmpty()) return back()->with('error', 'Produk tidak ditemukan.');
         
-        return view('Transaksi.create', compact('products'));
+        return view('Transaksi.create', compact('products', 'customers', 'outlets'));
     }
 }
--- resources/views/Transaksi/create.blade.php
+++ resources/views/Transaksi/create.blade.php
@@ -5,19 +5,100 @@
     <div class="flex flex-col justify-center items-center gap-y-5">
         <h1>Transaksi Penjualan</h1>
 
-        <form action="{{ route('penjualan.store') }}" method="POST" class="flex flex-col gap-y-5 items-center">
+        <form action="{{ route('penjualan.store') }}" method="POST" 
+        class="flex flex-col gap-y-5 p-2 items-center bg-blue-500">
             @csrf
 
-            <select name="">
-                @forelse ($products as $product)
-                    <option value="{{ $product->id }}">{{ $product->name }}</option>
-                @empty
-                    <p>Belum ada produk</p>
-                @endforelse
-            </select>
+            <div class="input-group flex justify-between w-full">
+                <label for="">Product ID</label>
+                <select name="product_id">
+                    @forelse ($products as $product)
+                        <option value="{{ $product->id }}">{{ $product->name . ' - ' . $product->price }}</option>
+                    @empty
+                        <p>Belum ada produk.</p>
+                    @endforelse
+                </select>
+            </div>
+            
+            {{-- invoice code otomatis --}}
+            
+            <div class="input-group flex justify-between w-full">
+                <label>Customer ID</label>
+                <select name="customer_id" >
+                    @forelse ($customers as $customer)
+                        <option value="{{ $customer->id }}">{{ $customer->name . '-' . $customer->status }}</option>
+                    @empty
+                        <option disabled>Belum ada customer.</option>
+                    @endforelse
+                </select>
+            </div>
 
-            <input type="number" name="quantity" min="1" max="10" required
-            class="border text-center">
+            <div class="input-group flex justify-between w-full">
+                <label>Outlet ID</label>
+                <select name="outlet_id">
+                    @forelse ($outlets as $outlet)
+                        <option value="{{ $outlet->id }}">{{ $outlet->name }}</option>
+                    @empty
+                        <option disabled>Belum ada outlet.</option>
+                    @endforelse
+                </select>
+            </div>
+
+            <div class="input-group flex justify-between w-full">
+                <label>Method</label>
+                <select name="method"  required>
+                    <option value="cash">Cash</option>
+                    <option value="qris">QRIS</option>
+                </select>
+            </div>
+            <div class="input-group flex justify-between w-full">
+                <label>Paid Amount</label>
+                <input type="number" name="paid_amount" min="0" required>
+            </div>
+            
+            <!-- Cash Change Otomatis -->
+
+            <div class="input-group flex justify-between w-full">
+                <label>Status</label>
+                <select name="status">
+                    <option value="pending">pending</option>
+                    <option value="paid">paid</option>
+                    <option value="cancel">cancel</option>
+                    <option value="refund">refund</option>
+                </select> 
+            </div>
+
+            <div class="input-group flex justify-between w-full">
+                <label>Quantity</label>
+                <input type="number" name="qty" min="1" max="10" required
+                class="border text-center">
+            </div>
+
+            {{-- <div class="input-group flex justify-between w-full">
+                <label>Price</label>
+                <input type="number" name="price" min="0" required
+                class="border text-center">
+            </div>
+
+            <div class="input-group flex justify-between w-full">
+                <label>Total</label>
+                <input type="number" name="qty" min="1" max="10" required
+                class="border text-center">
+            </div>
+             --}}
+
+            <div class="input-group flex justify-between w-full">
+                <label>Receipt Method</label>
+                <select name="receipt_method" required>
+                    <option value="print">Print</option>
+                    <option value="email">Email</option>
+                </select>
+            </div>
+
+            <div class="input-group flex justify-between w-full">
+                <label>Description</label>
+                <textarea name="description"></textarea>
+            </div>
 
             <button type="submit" class="bg-green-500 text-white font-semibold py-1 px-3 rounded">buy</button>
         </form>
